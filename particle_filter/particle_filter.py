c4f8ad3d80046ba608fadd5ab58b5156e2f78467
Kayla Comalli <Kayla.comalli@gmail.com>
2019-03-02 12:38:46 -0500

    drawing world with reference image

--- particle_filter/particle_filter.py
+++ particle_filter/particle_filter.py
@@ -18,7 +18,7 @@ class ParticleFilter():
 	def __init__(self, name, img):
 		self.name = name
 		self.img = img
-		self.orig_img = img
+		self.orig_img = img.copy()
 		self.rows = img.shape[0]
 		self.cols = img.shape[1]
 		self.iMap = ImgMap(self.img, DISTANCE_UNIT)
@@ -32,13 +32,13 @@ class ParticleFilter():
 		row = random.randint(0,self.rows-DISTANCE_UNIT)
 		s = self.iMap.selection(row, col, DISTANCE_UNIT)
 		self.img[s[0][0]:s[0][1], s[1][0]:s[1][1]] = [74, 69, 255]
-		self.state = self.iMap.origin(s)
+		self.state = s
 
 	def distribute_particles_randomly(self):
 		# TODO: give the particles a weight
 		particles = []
 		for p in range(M):
-			p = [random.randint(0,self.cols), random.randint(0,self.rows)]
+			p = [random.randint(0,self.cols), random.randint(0,self.rows), 1/M]
 			self.img[p[0]:p[0]+10, p[1]:p[1]+10] = [0, 0, 0]
 			particles.append(p)
 		return particles
@@ -47,23 +47,26 @@ class ParticleFilter():
 		while True:
 			cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
 			cv2.resizeWindow(self.name, self.cols, self.rows)
+			self.action_step()
 			self.get_measurement()
 			cv2.imshow(self.name, self.img)
-			self.action_step()
-			cv2.waitKey(0)
+			k = cv2.waitKey(33) 
+			if k == ord('r'):
+				cv2.imshow('measurement',self.measurement)
+			if k==27:
+				break
 
 	def get_measurement(self):
 		v = self.state
-		self.img[v[0][0]:v[0][1]+IMAGE[0], v[1][0]:v[1][1]+IMAGE[1]] = [0, 0 ,0]
-		self.measurement = [
-			[self.state[0][0], self.state[0][1]],
-			[self.state[1][0], self.state[1][1]],
-		]
+		self.measurement = self.orig_img[
+			v[0][0]-IMAGE[0]:v[0][1]+IMAGE[0],
+		  v[1][0]-IMAGE[0]:v[1][1]+IMAGE[1]].copy()
 
 	def resample(self):
+		# insert gaussian dist here
 		for p in self.X_t:
 			self.img[p[0]:p[0]+10, p[1]:p[1]+10] = self.orig_img[p[0]:p[0]+10, p[1]:p[1]+10] = [100, 100, 100]
-		self.distribute_particles_randomly()
+		# self.distribute_particles_randomly()
 
 	def action_step(self):
 		print('action step')
@@ -75,3 +78,6 @@ class ParticleFilter():
 		# if len(self.state[0]):
 		# 	for y in area[0]:
 		# 		for x in area[1]:
+
+def state_origin(self):
+	return self.iMap.origin(self.state)
