184f9b6d8a2ac3d5182dfb31afdd2c7ed7edaf52
Kayla Comalli <Kayla.comalli@gmail.com>
2019-03-02 16:07:45 -0500

    getting the correlation of all reference images

--- particle_filter/particle_filter.py
+++ particle_filter/particle_filter.py
@@ -12,10 +12,7 @@ from matplotlib import pyplot as plt
 
 IMAGE = [25, 25]
 DISTANCE_UNIT = 50
-M = 2000  # number of particles
-# Img Size: 27000000
-# Shape: [3000, 3000, 3]
-
+M = 2000 # number of particles
 
 class ParticleFilter():
 	def __init__(self, name, img):
@@ -28,7 +25,7 @@ class ParticleFilter():
 		self.state = [[], []]
 		self.set_state()
 		self.time_step = 0
-		self.get_reference_images()
+		self.get_refs()
 		self.distribute_particles_randomly()
 		self.store_histograms()
 
@@ -38,14 +35,6 @@ class ParticleFilter():
 		s = self.iMap.selection(row, col, DISTANCE_UNIT)
 		self.img[s[0][0]:s[0][1], s[1][0]:s[1][1]] = [74, 69, 255]
 		self.state = s
-
-	def distribute_particles_randomly(self):
-		particles = []
-		for p in range(M):
-			p = [random.randint(0, self.cols),random.randint(0, self.rows), 1/M]
-			self.img[p[0]:p[0]+10, p[1]:p[1]+10] = [0, 0, 0]
-			particles.append(p)
-		self.X_t = particles
 
 	def draw_world(self):
 		self.action_step()
@@ -65,21 +54,10 @@ class ParticleFilter():
 			elif k == 27:  # end processing
 				cv2.destroyAllWindows()
 				break
-			# elif chr(k) == 'm':  # show reference image
-			#   cv2.imshow('measurement', self.measurement)
+			elif chr(k) == 'm':  # show reference image
+			  cv2.imshow('measurement', self.measurement)
 			else:
 				k = 0
-
-	def resample(self):
-		# insert gaussian dist here
-		for p in self.X_t:
-			self.img[p[0]:p[0]+10, p[1]:p[1]+10] = self.orig_img[p[0]
-					:p[0]+10, p[1]:p[1]+10] = [100, 100, 100]
-		# self.distribute_particles_randomly()
-
-	def action_step(self):
-		print('action step')
-		self.revert_colors(self.state)
 
 	def revert_colors(self, area):
 		old_state = self.iMap.reset_origin(area)
@@ -89,14 +67,50 @@ class ParticleFilter():
 			v[0][0]-IMAGE[0]:v[0][1]+IMAGE[0],
 			v[1][0]-IMAGE[0]:v[1][1]+IMAGE[1]].copy()
 
-	def get_reference_images(self):
-		self.rImages = []
+	def get_origin(self, s):
+		return self.iMap.offset(s)
+
+################ REFS ######################
+
+	def get_refs(self):
+		self.refs = []
 		N = int(self.rows/DISTANCE_UNIT)
 		for row in range(N):
 			for col in range(N):
-				s = [[row*DISTANCE_UNIT, (row*DISTANCE_UNIT)+DISTANCE_UNIT],
-						[col*DISTANCE_UNIT, (col*DISTANCE_UNIT)+DISTANCE_UNIT]]
-				self.rImages.append(s)
+				ob = {
+					'loc': 'ref' + str(row) + str(col),
+					'image': [
+					[row*DISTANCE_UNIT, (row*DISTANCE_UNIT)+DISTANCE_UNIT],
+					[col*DISTANCE_UNIT, (col*DISTANCE_UNIT)+DISTANCE_UNIT]]
+				}
+				self.refs.append(ob)
+		self.store_histograms()
+
+	def store_histograms(self):
+		for k in self.refs:
+			v = k['image']
+			img = self.orig_img[v[0][0]:v[0][1], v[1][0]:v[1][1]]	
+			hist1 = cv2.calcHist([img], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+			hist1 = cv2.normalize(hist1, hist1).flatten()
+			k.update({
+				'loc': k['loc'],
+				'image': v,
+				'histogram': hist1
+			})
+
+	def compare_grams(self):
+		oG = cv2.calcHist([self.measurement], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+		oG = cv2.normalize(oG, oG).flatten()
+		for k in self.refs:
+			v = k['histogram']
+			coeff = cv2.compareHist(oG, v, cv2.HISTCMP_CORREL)
+			k.update({
+				'loc': k['loc'],
+				'image': k['image'],
+				'histogram': v,
+				'corr_coeff': coeff
+			})
+		self.refs.sort(key=lambda e: e['corr_coeff'], reverse=True)
 
 	def ref_histogram(self, s):
 		plt.hist(s.ravel(),256,[0,256])
@@ -104,23 +118,37 @@ class ParticleFilter():
 		# plt.show()
 		# plt.pause(0.03)
 
-	def store_histograms(self):
-		self.grams = []
-		for i in self.rImages:
-			img = self.img[i[0][0]:i[0][1], i[1][0]:i[1][1]]	
-			hist1 = cv2.calcHist([img], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-			hist1 = cv2.normalize(hist1, hist1).flatten()
-			self.grams.append(hist1)
-		print(len(self.grams))
-		print(self.grams[0])
+##################### /REFS ######################
 
-	def compare_grams(self):
-		oG = cv2.calcHist([self.measurement], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-		oG = cv2.normalize(oG, oG).flatten()
-		results = []
-		for g in self.grams:
-			results.append(cv2.compareHist(oG, g, cv2.HISTCMP_CORREL))
-		return results
 
-	def get_origin(self, s):
-		return self.iMap.offset(s)
+################# PARTICLES ######################
+
+	def distribute_particles_randomly(self):
+		particles = []
+		for p in range(M):
+			p = [random.randint(0, self.cols),random.randint(0, self.rows), 1/M]
+			self.img[p[0]:p[0]+10, p[1]:p[1]+10] = [0, 0, 0]
+			particles.append(p)
+		self.X_t = particles
+
+	def resample(self):
+		# insert gaussian dist here
+		for p in self.X_t:
+			self.img[p[0]:p[0]+10, p[1]:p[1]+10] = self.orig_img[p[0]
+					:p[0]+10, p[1]:p[1]+10] = [100, 100, 100]
+		# self.distribute_particles_randomly()
+
+################# /PARTICLES ####################
+
+################# MOVEMENT #####################
+
+	def action_step(self):
+		print('action step')
+		self.revert_colors(self.state)
+
+################# /MOVEMENT ####################
+
+
+################ PREDICTION #####################
+
+################ /PREDICTION #####################
