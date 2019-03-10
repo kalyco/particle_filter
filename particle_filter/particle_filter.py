6fd66ecbdff10733195ab4a172fca687bec82a66
Kayla Comalli <Kayla.comalli@gmail.com>
2019-03-10 16:50:16 -0400

    fix: added noise in proper place

--- particle_filter/particle_filter.py
+++ particle_filter/particle_filter.py
@@ -40,7 +40,8 @@ class ParticleFilter():
 			elif chr(k) == '2': # Inflate particles
 				self.inflate()
 			elif chr(k) == '3': # Resample set 
-				self.resample()
+				# self.roulette_wheel_resample()
+				self.distance_based_resample()
 			elif chr(k) == '4': # Move bot
 				self.move_agent()
 			elif chr(k) == '5': # Shift particles	
@@ -55,7 +56,7 @@ class ParticleFilter():
 		self.update_state(self.dt == 0)
 		self.dist_particles()
 		self.inflate()
-		self.resample()
+		self.roulette_wheel_resample()
 		self.move_agent()
 		self.shift_particles()
 
@@ -63,8 +64,8 @@ class ParticleFilter():
 
 	def update_state(self, init=False): # 0
 		if (init):
-			col = random.randint(0, self.cols-DU)
-			row = random.randint(0, self.rows-DU)
+			col = random.randint(REF_U, self.cols-DU)
+			row = random.randint(REF_U, self.rows-DU)
 			self.state = self.iMap.offset_vector(row,col)
 			s = self.iMap.selection(row, col, REF_U)
 		else:
@@ -76,6 +77,7 @@ class ParticleFilter():
 ################# Step 1: Random Particles ####################
 
 	def dist_particles(self): # 1
+		self.redraw_world()
 		self.P = []
 		for i in range(M):
 			col = random.randint(0, self.cols-DU)
@@ -86,6 +88,28 @@ class ParticleFilter():
 
 ################ Step 2: Inflate Particles #######################
 
+	def distance_based_resample(self):
+		self.redraw_world()
+		weightSum = 0
+		for m in self.P:
+			diff = self.avg_dist(m['state'], self.state)
+			m['weight'] = 1 - diff
+		wheelVals = []
+		total = sum(list(map(lambda x: x['weight'], self.P)))
+		noise = np.random.normal(0, 1.0, M)
+		for i in range(M):	
+			weightSum += self.P[i]['weight']
+			wheelVals.append(weightSum/total)
+			x = random.random()
+			idx = bisect.bisect(wheelVals, x)
+			self.P[i] = self.P[idx]
+			self.P[i]['state'][0] += int(noise[i])
+			self.P[i]['state'][1] += int(noise[i])
+			self.redraw_point(self.P[i]['state'], 8)
+
+	def avg_dist(self, s1, s2):
+		return abs(s1[0] - s2[0]) + abs(s1[1] - s2[1])/2
+
 	def compare_grams(self): # 2.a
 		oG = cv2.calcHist([self.get_measurement(self.state)],
 		 [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
@@ -93,6 +117,9 @@ class ParticleFilter():
 		for p in self.P:
 			v = self.get_grams(p)
 			coeff = cv2.compareHist(oG, v, cv2.HISTCMP_CORREL)
+			if (p['state'][0] > 1500 or p['state'][0] < -1500 or
+			 p['state'][1] > 1500 or p['state'][1] < -1500):
+				coeff = 0
 			p.update({'state': p['state'], 'prior': p['prior'], 'weight': coeff})
 		self.P.sort(key=lambda e: e['weight'], reverse=True)
 
@@ -103,32 +130,28 @@ class ParticleFilter():
 
 	def inflate(self): # 2.c
 		self.compare_grams()
-		self.img = self.orig_img.copy()
-		self.update_state()
+		self.redraw_world()
 		for p in self.P:
-			mid_rad = 12
+			mid_rad = 40
 			radius = np.floor(mid_rad + p['weight'] * mid_rad)
 			self.redraw_point(p['state'], int(radius))
 
 #################### Step 3: Resample particles #######################
 
-	def resample(self): # 3
-		self.img = self.orig_img.copy()
-		self.update_state()
-		self.roulette_wheel_resample()
-
 	def roulette_wheel_resample(self): # 3.a
+		self.redraw_world()
 		wheelVals = []
 		weightSum = 0
 		total = sum(list(map(lambda x: x['weight'], self.P)))
-		self.img = self.orig_img.copy()
-		self.update_state()
+		noise = np.random.normal(0, 1.0, M)
 		for i in range(M):
 			weightSum += self.P[i]['weight']
 			wheelVals.append(weightSum/total)
 			x = random.random()
 			idx = bisect.bisect(wheelVals, x)
-			self.P[i]['state'] = self.P[idx]['state']
+			self.P[i] = self.P[idx]
+			self.P[i]['state'][0] += int(noise[i])
+			self.P[i]['state'][1] += int(noise[i])
 			self.redraw_point(self.P[i]['state'], 8)
 
 ################### Step 4: Move agent #######################
@@ -142,14 +165,18 @@ class ParticleFilter():
 		self.gen_control()
 		self.dt += 1
 		self.state = self.get_movement(self.state)
-		self.update_state()
+		self.actual_state = [ # agent does not have access to noise 
+			self.state[0] + int(np.random.normal(0, 1.0)),
+			self.state[0] + int(np.random.normal(0, 1.0)),
+		]
+		self.redraw_world()
+		for p in self.P:
+			self.redraw_point(p['state'], 8)
 
 ################### Step 5: Shift particles #######################
 
 	def shift_particles(self): # 5
-		self.img = self.orig_img.copy()
-		self.update_state()
-		print(len(self.P))
+		self.redraw_world()
 		for p in self.P:
 			p['state'] = self.get_movement(p['state'])
 			self.redraw_point(p['state'], 8)
@@ -164,9 +191,13 @@ class ParticleFilter():
 	
 	def redraw_point(self, s, radius):
 		row,col = self.iMap.to_image(s[0], s[1])
-		cv2.circle(self.img, (col, row), radius, (75,0,130), thickness=8)
+		cv2.circle(self.img, (col, row), radius, (0,0,0), thickness=8)
 
 	def get_movement(self, state):
-		state[0] += int(self.control[0] + np.random.normal(0, 1.0, 3000)[0]) # agent does not have access to noise
-		state[1] += int(self.control[1] + np.random.normal(0, 1.0, 3000)[0])
+		state[0] += int(self.control[0])
+		state[1] += int(self.control[1])
 		return state
+
+	def redraw_world(self):
+		self.img = self.orig_img.copy()
+		self.update_state()
